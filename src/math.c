135fe2f5a5c6ecc5a9d3bfe5a3e6c94942b20a83
anilalis <anildervisogluib@gmail.com>
2023-02-17 23:37:51 +0300

    rotations fixed

--- src/math.c
+++ src/math.c
@@ -1,91 +1,36 @@
 #include "fdf.h"
 
-void	rotate_x(t_dot **matrix, t_fdf *data, double alpha)
+void rotation_x(t_dot **matrix, t_fdf *data, int negative)
 {
-    int i;
-    int j;
-	double	prev_y;
+    int x;
+    int y;
+    double yz_angle;
+    double radius;
+    double bir = 2 * atan(1);
 
-    j = 0;
-
-    while (j < data->matrix_heigth)
+    y = 0;
+    while (y < data->matrix_heigth)
     {
-        i = 0;
-        while (i < data->matrix_width)
+        x = 0;
+        while (x < data->matrix_width)
         {
-            prev_y = matrix[j][i].y;
-	        matrix[j][i].y = prev_y * cos(alpha) + (matrix[j][i].z) * sin(alpha);
-	        matrix[j][i].z = -prev_y * sin(alpha) + (matrix[j][i].z) * cos(alpha);
-            i++;
+            matrix[y][x].y *= -1;
+            radius = hypotenuse(0.0, (matrix[y][x].y), (matrix[y][x].z));
+            if (radius == 0)
+                yz_angle = 0;
+            else
+                yz_angle = asin(matrix[y][x].y / radius);
+            if (matrix[y][x].z < 0.0)
+                yz_angle = (2 * bir) - yz_angle;
+            yz_angle += data->rotation_value_x * negative;
+            matrix[y][x].z = radius * cos(yz_angle);
+            matrix[y][x].y = radius * sin(yz_angle);
+            matrix[y][x].y *= -1;
+            x++;
         }
-        j++;
+        y++;
     }
 }
-
-// static void	rotate_y(int *x, int *z, double beta)
-// {
-// 	int	prev_x;
-
-// 	prev_x = *x;
-// 	*x = prev_x * cos(beta) + (*z) * sin(beta);
-// 	*z = -prev_x * sin(beta) + (*z) * cos(beta);
-// }
-
-void	rotate_z(t_dot **matrix, t_fdf *data, double gamma)
-{
-    int i = 0;
-    int j = 0;
- 	double	prev_x;
- 	double prev_y;
-
-    while (j < data->matrix_heigth)
-    {
-        i = 0;
-        while (i < data->matrix_width)
-        {
-            prev_x = matrix[j][i].x;
-            prev_y = matrix[j][i].y;
-            matrix[j][i].x = prev_x * cos(gamma) - prev_y * sin(gamma);
-            matrix[j][i].y = prev_x * sin(gamma) + prev_y * cos(gamma);
-            i++;
-        }
-        j++;
-    }
-}
-
-// void rotation_x(t_dot **matrix, t_fdf *data, int negative)
-// {
-//     int x;
-//     int y;
-//     double yz_angle;
-//     double radius;
-//     double bir = 2 * atan(1);
-
-//     y = 0;
-//     while (y < data->matrix_heigth)
-//     {
-//         x = 0;
-//         while (x < data->matrix_width)
-//         {
-//             matrix[y][x].y *= -1;
-//             radius = hypotenuse(0.0, (matrix[y][x].y), (matrix[y][x].z));
-//             if (radius == 0)
-//                 yz_angle = 0;
-//             else
-//                 yz_angle = asin(matrix[y][x].y / radius);
-//             if (matrix[y][x].z < 0.0)
-//                 yz_angle = (2 * bir) - yz_angle;
-//             yz_angle += data->rotation_value_x * negative;
-//             matrix[y][x].z = radius * cos(yz_angle);
-//             matrix[y][x].y = radius * sin(yz_angle);
-//             matrix[y][x].y *= -1;
-//             x++;
-//         }
-//         y++;
-//     }
-// }
-
-
 
 void rotation_y(t_dot **matrix, t_fdf *data, int negative)
 {
@@ -117,48 +62,48 @@ void rotation_y(t_dot **matrix, t_fdf *data, int negative)
     }
 }
 
-// void rotation_z(t_dot **matrix, t_fdf *data, int negative)
-// {
-//     int x;
-//     int y;
-//     double xy_angle;
-//     double radius;
-//     double bir = 2 * atan(1);
+void rotation_z(t_dot **matrix, t_fdf *data, int negative)
+{
+    int x;
+    int y;
+    double xy_angle;
+    double radius;
+    double bir = 2 * atan(1);
 
-//     y = 0;
-//     while (y < data->matrix_heigth)
-//     {
-//         x = 0;
-//         while (x < data->matrix_width)
-//         {
-//             matrix[y][x].y *= -1.0;
-//             radius = hypotenuse((matrix[y][x].x), (matrix[y][x].y), 0.0);
-//             if (radius == 0)
-//                 xy_angle = 0;
-//             else
-//                 xy_angle = asin(matrix[y][x].y / radius);
-//             if (matrix[y][x].x < 0.0)
-//                 xy_angle = (2 * bir) - xy_angle;
-//             xy_angle += data->rotation_value_z * negative;
-//             matrix[y][x].x = radius * cos(xy_angle);
-//             matrix[y][x].y = radius * sin(xy_angle);
-//             matrix[y][x].y *= -1.0;
-//             x++;
-//         }
-//         y++;
-//     }
-// }
+    y = 0;
+    while (y < data->matrix_heigth)
+    {
+        x = 0;
+        while (x < data->matrix_width)
+        {
+            matrix[y][x].y *= -1.0;
+            radius = hypotenuse((matrix[y][x].x), (matrix[y][x].y), 0.0);
+            if (radius == 0)
+                xy_angle = 0;
+            else
+                xy_angle = asin(matrix[y][x].y / radius);
+            if (matrix[y][x].x < 0.0)
+                xy_angle = (2 * bir) - xy_angle;
+            xy_angle += data->rotation_value_z * negative;
+            matrix[y][x].x = radius * cos(xy_angle);
+            matrix[y][x].y = radius * sin(xy_angle);
+            matrix[y][x].y *= -1.0;
+            x++;
+        }
+        y++;
+    }
+}
 
 void translation(t_fdf *data, int axis)
 {
     if (axis == 123)
-        data->translation_x -= 50;
+        data->translation_x -= data->window_width / 20;
     else if (axis == 124)
-        data->translation_x += 50;
+        data->translation_x += data->window_width / 20;
     else if (axis == 125)
-        data->translation_y += 50;
+        data->translation_y += data->window_heigth / 20;
     else if (axis == 126)
-        data->translation_y -= 50;
+        data->translation_y -= data->window_heigth / 20;
 }
 
 void zoom(t_fdf *data, int val)
