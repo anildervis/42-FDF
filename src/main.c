f4f69de00149bdadc11b79f36e6be6a3f990f215
anilalis <anildervisogluib@gmail.com>
2023-02-18 02:22:32 +0300

    just left with initial setup and necessary functions, also with some ideas

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ void setup(t_fdf *data)
     data->window_width = 1000;
     data->mlx_ptr = mlx_init();
     data->win_ptr = mlx_new_window(data->mlx_ptr, data->window_heigth, data->window_width, "FDF");
+    data->img_ptr = mlx_new_image(data->mlx_ptr, data->window_heigth, data->window_width);
     data->zoom = 1;
     data->translation_x = 0;
     data->translation_y = 0;
@@ -14,59 +15,24 @@ void setup(t_fdf *data)
     data->matrix_min = 2147483647;
     data->max_color = 0x2978ff;
     data->min_color = 0x28ff5a;
-    data->scale = 3;
-    data->rotation_value_x = 0.5;
-    data->rotation_value_y = 0.5;
-    data->rotation_value_z = 0.5;
+    data->scale = 2;
+    data->rotation_value_x = 0.1;
+    data->rotation_value_y = 0.1;
+    data->rotation_value_z = 0.1;
     data->heigth = 0.01;
+    data->buffer = mlx_get_data_addr(data->img_ptr, &data->pixel_bits, &data->line_bytes, &data->endian); // başka yere konulabilir
 }
 
-// ptos -> plane to spherical
-void ptos(t_fdf *data, t_dot **matrix)
-{
-    mlx_clear_window(data->mlx_ptr, data->win_ptr);
-    double pi = 3.14159265359;
-    double radius = data->matrix_heigth / 2;
-    double heigth;
-    int i = 0;
-    int j = 0;
-    double y_angle = pi / (data->matrix_heigth - 1);
-    double xz_angle = 2 * pi / (data->matrix_width - 1);
-    while (j < data->matrix_heigth)
-    {
-        i = 0;
-        while (i < data->matrix_width)
-        {
-            heigth = radius + matrix[j][i].z * data->heigth;
-            matrix[j][i].y = (-1) * heigth * cos(y_angle * j);
-            matrix[j][i].x = heigth * sin(y_angle * j) * (-sin(xz_angle * i));
-            matrix[j][i].z = heigth * sin(y_angle * j) * (-cos(xz_angle * i));
-            i++;
-        }
-        j++;
-    }
-}
 
-// stop -> spherical to plane
-// void stop(t_dot **matrix, t_fdf *data)
 
-void colorize(t_fdf *data, t_dot **matrix)
-{
-    int i;
-    int j;
+/* 
+herhangi bir hareket uygulandığı zaman duracak,
+basıldığı zaman otomatik oynatma moduna geçecek ve
+window köşeleri dahilinde random hareket edicek
+*/
 
-    i = 0;
-    while (i < data->matrix_heigth)
-    {
-        j = 0;
-        while (j < data->matrix_width)
-        {
-            matrix[i][j].color = gradient(data->min_color, data->max_color, data->matrix_max - data->matrix_min, matrix[i][j].z - data->matrix_min);
-            j++;
-        }
-        i++;
-    }
-}
+// makefile SRC wildcard kullanımı yasak
+// makfile için renklendirme eklenebilir
 
 int main(int argc, char **argv)
 {
@@ -78,13 +44,10 @@ int main(int argc, char **argv)
     data = (t_fdf*)malloc(sizeof(t_fdf));
     setup(data);
     matrix = read_data(data, argv[1]);
-    colorize(data, matrix);
-    // belirlenen keycode ile color değerlerini sonradan değiştirebilmemiz lazım
-    // keycode ile esc = quit
+    data->matrix = matrix;
     ptos(data, matrix);
     print_all(matrix, data);
     // print menu
-    data->matrix = matrix;
     mlx_hook(data->win_ptr, 2, 1L<<0, key_hook, data);
     mlx_loop(data->mlx_ptr);
 }
