60bf8d7cd92022cdb5ab5dbf709335087aac3aa0
anilalis <anildervisogluib@gmail.com>
2023-02-18 00:04:33 +0300

    drawing is done by putting pixels on image and drawing that image on given coordinate, gradient function is finalized, background function can be improved by bonuses (like adding starts)

--- src/draw.c
+++ src/draw.c
@@ -1,60 +1,44 @@
 #include "fdf.h"
 
-//gradient fonksiyonu
-# define R(a) (a) >> 16
-# define G(a) ((a) >> 8) & 0xFF
-# define B(a) (a) & 0xFF
-
-int gradient_1(int startcolor, int endcolor, double len, int step)
+int gradient(int start, int end, double len, int step)
 {
-    double increment[3];
+    double i[3];
     int red;
     int green;
     int blue;
 
-    if (startcolor == endcolor)
-        return (startcolor);
+    if (start == end)
+        return (start);
 
-    increment[0] = (double)((R(endcolor)) - (R(startcolor))) / (double)len;
-    increment[1] = (double)((G(endcolor)) - (G(startcolor))) / (double)len;
-    increment[2] = (double)((B(endcolor)) - (B(startcolor))) / (double)len;
+    i[0] = (double)(((end >> 16)) - ((start >> 16))) / (double)len;
+    i[1] = (double)(((end >> 8) & 0xFF) - ((start >> 8) & 0xFF)) / (double)len;
+    i[2] = (double)(((end & 0xFF)) - ((start & 0xFF))) / (double)len;
 
-    red = (R(startcolor)) + ft_round(step * increment[0]);
-    green = (G(startcolor)) + ft_round(step * increment[1]);
-    blue = (B(startcolor)) + ft_round(step * increment[2]);
+    red = ((start >> 16)) + ft_round(step * i[0]);
+    green = ((start >> 8) & 0xFF) + ft_round(step * i[1]);
+    blue = ((start & 0xFF)) + ft_round(step * i[2]);
     return ((red << 16) | (green << 8) | (blue));
 }
 
-int gradient_2(int startcolor, int endcolor, double len, int step)
+void put_pixel(t_fdf *data, int x_point, int y_point, int color)
 {
-    double increment[3];
-    int red;
-    int green;
-    int blue;
+    int pixel;
 
-    if (startcolor == endcolor)
-        return (startcolor);
-
-    increment[0] = (double)(((endcolor >> 16)) - ((startcolor >> 16))) / (double)len;
-    increment[1] = (double)(((endcolor >> 8) & 0xFF) - ((startcolor >> 8) & 0xFF)) / (double)len;
-    increment[2] = (double)(((endcolor & 0xFF)) - ((startcolor & 0xFF))) / (double)len;
-
-    red = ((startcolor >> 16)) + ft_round(step * increment[0]);
-    green = ((startcolor >> 8) & 0xFF) + ft_round(step * increment[1]);
-    blue = ((startcolor & 0xFF)) + ft_round(step * increment[2]);
-    return ((red << 16) | (green << 8) | (blue));
-}
-
-int gradient(int startcolor, int endcolor, double len, int step)
-{
-    int red;
-    int green;
-    int blue;
-
-    red = ((startcolor >> 16)) + ft_round(step * (double)(((endcolor >> 16)) - ((startcolor >> 16))) / (double)len);
-    green = ((startcolor >> 8) & 0xFF) + ft_round(step * (double)(((endcolor >> 8) & 0xFF) - ((startcolor >> 8) & 0xFF)) / (double)len);
-    blue = ((startcolor & 0xFF)) + ft_round(step * (double)(((endcolor & 0xFF)) - ((startcolor & 0xFF))) / (double)len);
-    return ((red << 16) | (green << 8) | (blue));
+    pixel = ((int)y_point * data->line_bytes) + ((int)x_point * 4);
+    if (data->endian == 1)        // Most significant (Alpha) byte first
+    {
+        data->buffer[pixel + 0] = (color >> 24);
+        data->buffer[pixel + 1] = (color >> 16) & 0xFF;
+        data->buffer[pixel + 2] = (color >> 8) & 0xFF;
+        data->buffer[pixel + 3] = (color) & 0xFF;
+    }
+    else if (data->endian == 0)   // Least significant (Blue) byte first
+    {
+        data->buffer[pixel + 0] = (color) & 0xFF;
+        data->buffer[pixel + 1] = (color >> 8) & 0xFF;
+        data->buffer[pixel + 2] = (color >> 16) & 0xFF;
+        data->buffer[pixel + 3] = (color >> 24);
+    }
 }
 
 void draw_line(t_dot a, t_dot b, t_fdf *data)
@@ -65,6 +49,7 @@ void draw_line(t_dot a, t_dot b, t_fdf *data)
     double y_point;
     double max_step;
     int step;
+    int color;
 
     step = 0;
     x_point = (a.x + data->window_width / 2 + data->translation_x);
@@ -74,52 +59,50 @@ void draw_line(t_dot a, t_dot b, t_fdf *data)
     max_step = fmax(fabs(x_step), fabs(y_step));
     x_step /= max_step;
     y_step /= max_step;
-    // 777 Hızlı çalış okudum kabulettim inandım 777;
-    if (a.color == b.color)
+    while (step++ < max_step)
     {
-        while (step++ < max_step)
-        {
-            mlx_pixel_put(data->mlx_ptr, data->win_ptr, x_point, y_point, a.color);
-            x_point += x_step;
-            y_point += y_step;
-            // if (x_point > data->window_width || y_point > data->window_heigth)
-            //     break;
-        }
-    }
-    else
-    {
-        while (step < max_step)
-        {
-            mlx_pixel_put(data->mlx_ptr, data->win_ptr, x_point, y_point, gradient(a.color, b.color, max_step, step++));
-            x_point += x_step;
-            y_point += y_step;
-            if (x_point > data->window_width || y_point > data->window_heigth)
-                break;
-        }
+        color = gradient(a.color, b.color, max_step, step);
+        if (data->pixel_bits != 32)
+            color = mlx_get_color_value(data->mlx_ptr, color);
+        put_pixel(data, x_point, y_point, color);
+        x_point += x_step;
+        y_point += y_step;
     }
 }
 
-
-// isometric fonksiyonu
-void	isometric(t_dot *dot, double angle)
+// if some key is pressed stars can be put on the background
+void put_background(t_fdf *data)
 {
-	dot->x = (dot->x - dot->y) * cos(angle);
-	dot->y = (dot->x + dot->y) * sin(angle) - dot->z;
+    int color;
+    int x;
+    int y;
+    
+    x = 0;
+    y = 0;
+    color = 0x000000;
+    if (data->pixel_bits != 32)
+        color = mlx_get_color_value(data->mlx_ptr, color);
+    while (y < data->window_heigth) // img struct yapısı ile image için size bilgileri, başlangıç koordinati vb. tutulabilir 
+    {
+        while (x < data->window_width)
+            put_pixel(data, x, y, color);
+    } 
+    mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_ptr, 0, 0);
 }
 
-// spherical ve normal print diye ayrılabilir / print_all için yeni bir parametre eklenebilir
 void print_all(t_dot **matrix, t_fdf *data)
 {
     int x;
     int y;
 
+    put_background(data);
     y = 0;
     while (y < data->matrix_heigth)
     {
         x = 0;
         while (x < data->matrix_width)
         {
-            if (matrix[y][x].z >= 0)
+            if (matrix[y][x].z >= 0) // || if !(image_is_spherical(t_render diye struct?)) 
             {
                 if (x != data->matrix_width - 1)
                     draw_line(matrix[y][x], matrix[y][x + 1], data);
@@ -130,4 +113,5 @@ void print_all(t_dot **matrix, t_fdf *data)
         }
         y++;
     }
+    mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_ptr, 0, 0);
 }
